37523a7d25d293cdd29de1697f4da64574de9c16
Maycol Arevalo <marevalob1@miumg.edu.gt>
2022-10-13 06:56:03 -0600

    2

--- src/app/Persona/seats/seats.component.ts
+++ src/app/Persona/seats/seats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-seats',
@@ -7,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeatsComponent implements OnInit {
 
+
+
+ AndyFilas:number[]  =[1,2,3];
+
+ AndyColumna:number[]  =[1,2,3,4,5,6];
+ AndyAsientos:string[]  =["a","b","c","d","e","f"];
+
+  idAsiento:String[]=["aa","ab","ba","bb","ca","cb"];
+ row:string[] = ['row row--1','row row--2','row row--3','row row--4']
+
+ contar:number=this.get();
+ public get():any{
+   this.AndyFilas.forEach(element => {this.AndyAsientos.length+this.AndyColumna.length
+     return element  
+   });
+   
+ }
   constructor() { }
+
+
+/*
+generarMatriz(){
+  let TotalAsientos:number=this.AndyFilas.length*this.AndyColumna.length;
+  let idAsiento:String[6]=["aa","ab","ba","bb","ca","cb"];
+
+ contador:number=0;
+    for(let j:number=0; j<this.AndyColumna.length;j++){
+      this.idAsiento[0]=this.AndyFilas[i]+""+this.AndyColumna[j];
+      contador++;
+    } 
+  }
+
+
+
+}
+*/
 
   ngOnInit(): void {
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {DataService} from './data.service'
 })
 export class AppComponent {
   title = 'vuelosVenta';
-  
+  usuarios  =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
   posts: any;
   constructor(private router:Router, private dataService:DataService){

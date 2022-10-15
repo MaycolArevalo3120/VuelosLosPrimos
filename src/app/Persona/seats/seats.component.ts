de2961cf451bfd5c23d89f7fa2ab434114149955
TheoBloem-Auxiliar Desarrollo <105243275+TheoBloem-AuxiliarDesarrollo@users.noreply.github.com>
2022-10-14 19:12:12 -0600

    UI-Add

--- src/app/Persona/seats/seats.component.ts
+++ src/app/Persona/seats/seats.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-seats',
@@ -9,43 +8,58 @@ import { elementAt } from 'rxjs';
 export class SeatsComponent implements OnInit {
 
 
-
- AndyFilas:number[]  =[1,2,3];
-
- AndyColumna:number[]  =[1,2,3,4,5,6];
- AndyAsientos:string[]  =["a","b","c","d","e","f"];
-
-  idAsiento:String[]=["aa","ab","ba","bb","ca","cb"];
- row:string[] = ['row row--1','row row--2','row row--3','row row--4']
-
- contar:number=this.get();
- public get():any{
-   this.AndyFilas.forEach(element => {this.AndyAsientos.length+this.AndyColumna.length
-     return element  
-   });
-   
- }
   constructor() { }
-
-
-/*
-generarMatriz(){
-  let TotalAsientos:number=this.AndyFilas.length*this.AndyColumna.length;
-  let idAsiento:String[6]=["aa","ab","ba","bb","ca","cb"];
-
- contador:number=0;
-    for(let j:number=0; j<this.AndyColumna.length;j++){
-      this.idAsiento[0]=this.AndyFilas[i]+""+this.AndyColumna[j];
-      contador++;
-    } 
-  }
-
-
-
-}
-*/
-
   ngOnInit(): void {
+    this.numeroFilas = 10
+    this.numeroColumnas = 6
+    this.ingresoDeFilasColumas()
+    this.estadoAsiento=Array.from(Array(this.numeroFilas), () => new Array(this.numeroColumnas).fill(0))
+    this.asientosPreCargados()
   }
+
+  asientosPreCargados(){
+    this.estadoAsiento[1][2] = 1
+    this.estadoAsiento[1][5] = 1
+    this.estadoAsiento[2][4] = 1
+    this.estadoAsiento[6][2] = 1
+    this.estadoAsiento[4][2] = 1
+  }
+  
+
+  numeroFilas:number=0;
+  numeroColumnas:number=0;
+  
+  ingresoDeFilasColumas(){
+    this.Filas = Array.from({length: this.numeroFilas}, (_, i) => i + 1)
+    this.Columna = Array.from({length: this.numeroColumnas}, (_, i) => i + 1)
+  }
+  Filas:number[]  =[];
+  estadoAsiento:any[][]=[];
+  Columna:number[]  =[];
+
+  crearID(fila: String, columna: String): String {
+    fila = fila + "-";
+    return fila.concat(columna.toString());
+  }
+
+  guardarEstadoAsiento(evento:any){    
+    console.log(evento.srcElement.id)
+    this.cambiarEstadoAsiento(evento.srcElement.id)
+    console.log(this.estadoAsiento)
+    
+  }
+
+  cambiarEstadoAsiento(idasiento:any){
+    //11-5
+    const id =idasiento.split("-")
+    let fila = parseInt(id[0])
+    let columna = parseInt(id [1])
+    let estadoActual = this.estadoAsiento[fila-1][columna-1]
+    if (estadoActual == 0)
+      this.estadoAsiento[fila-1][columna-1] = 1
+    else
+    this.estadoAsiento[fila-1][columna-1] = 0
+  }
+
 
 }
